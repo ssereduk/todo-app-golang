cdeccb553e86cab00016a42c1429c9cea478d427
Stas Seredjuk <spsereduk3891@gmail.com>
2022-05-28 02:59:38 +0300

    second commit

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"log"
+
+	"github.com/ssereduk/todo-app-golang"
+
+	"github.com/ssereduk/todo-app-golang/pkg/handler"
 )
 
-
 func main() {
-	srv:= new(todo.Server)
-	if err := srv.Run("8000"); err != nill {
-		log.Fatalf("error occured whilen running http server: %s, err.Error()")
+	handlers := new(handler.Handler{})
+	srv := new(todo.Server)
+	if err := srv.Run("8000"); err != nil {
+		log.Fatalf("error occured whilen running http server: %s", err.Error())
 	}
 }
