08dadbe4c04f6a53d40c4bbaacb22d6eef4c3dd5
Stas Seredjuk <spsereduk3891@gmail.com>
2022-05-28 03:32:41 +0300

    3 commit

--- cmd/main.go
+++ cmd/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	handlers := new(handler.Handler{})
+	handlers := new(handler.Handler)
 	srv := new(todo.Server)
 	if err := srv.Run("8000"); err != nil {
 		log.Fatalf("error occured whilen running http server: %s", err.Error())
